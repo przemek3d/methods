cf4dc3da762ec52769c414537231b7206dca9ee9
przemek3d <90689584+przemek3d@users.noreply.github.com>
2022-01-18 13:31:42 +0100

    teee
    eeee

---
+++ main.js
@@ -0,0 +1,10 @@
+const method = {};
+const bruh = "essa";
+const button = document.querySelector(".button func");
+let text = document.querySelector(".text");
+let args = document.querySelector(".args");
+
+function test() {
+    console.log(text);
+    console.log(args);
+}
